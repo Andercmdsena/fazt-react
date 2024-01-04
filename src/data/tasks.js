90c4b13c6dd7abdd62464b71fb72e50907ebb585
Andercmdsena <andercmdsena>
2024-01-04 11:03:00 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id:0,
+        title:'mi primera chamba',
+        descripcion: 'mi primer tarea'
+    },
+    {
+        id:2,
+        title:'mi segunda chamba',
+        descripcion: 'mi segunda tarea'
+    },
+    {
+        id:3,
+        title:'mi tercera chamba',
+        descripcion: 'mi tercera tarea'
+    }
+]
